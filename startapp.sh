002d70b65570ceb087fe722a89d7664102a6adf0
qnistar <111226117+qnistar@users.noreply.github.com>
2025-04-08 20:21:37 +0900

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -3,7 +3,8 @@
 #export LC_ALL="en.utf-8"
 export LC_ALL=C
 export HOME=/config
-#ls /usr/bin/grass && dpkg -i /grass/grass.deb && sleep 3s
+
 exec /usr/bin/grass
 
+#ls /usr/bin/grass && dpkg -i /grass/grass.deb && sleep 3s
 #sleep 7s ; ls /config/xdg/data/io.getgrass.desktop/localstorage && wmctrl -r grass -b add,hidden   
