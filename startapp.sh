3c0ce137420b4e860146a2b984ec8a443dd96c12
qnistar <111226117+qnistar@users.noreply.github.com>
2025-12-24 10:56:06 +0900

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -8,7 +8,7 @@ exec /usr/bin/grass
 
 sleep 1s
 
-bash wmctrl_retry.sh
+bash wmctrl_retry.sh   # hidden   
 
 #ls /usr/bin/grass && dpkg -i /grass/grass.deb && sleep 3s
 #sleep 7s ; ls /config/xdg/data/io.getgrass.desktop/localstorage && wmctrl -r grass -b add,hidden   
