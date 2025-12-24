ee93ee45f5d3440b898e02f31dae6bf8d93b67f9
qnistar <111226117+qnistar@users.noreply.github.com>
2025-12-24 15:51:17 +0900

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -5,22 +5,30 @@
 export HOME=/config
 mkdir -p /config
 
+
+# 4. 컨테이너 유지
+echo "Container is running..."
+tail -f /dev/null
+
+
+
 # 2. Xvfb 가상 디스플레이 시작
 # :99 디스플레이를 생성하고 해상도를 설정합니다.
-Xvfb :99 -screen 0 1280x1024x24 &
-export DISPLAY=:99
+#Xvfb :99 -screen 0 1280x1024x24 &
+#export DISPLAY=:99
 
 
 # 2. 윈도우 매니저 실행 (창 크기 조절 등을 위해 필요)
-openbox &
+#openbox &
 
 # 3. Grass 앱 실행 (경로 확인 필요, 보통 /usr/bin/grass)
 # 만약 root 권한 문제 발생 시 --no-sandbox 옵션 추가
-/usr/bin/grass --no-sandbox &
+#/usr/bin/grass --no-sandbox &
 
 # 4. wmctrl 등을 이용한 추가 스크립트 실행 (필요한 경우)
-sleep 5
+#sleep 5
 #/wmctrl_retry.sh &
 
 # 5. 프로세스가 종료되지 않도록 유지
-wait
+#wait
+
