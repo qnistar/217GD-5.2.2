a80ca2c4bef3683ac45bbcd81edebd4db603dd20
qnistar <111226117+qnistar@users.noreply.github.com>
2025-12-25 09:37:42 +0900

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -4,7 +4,10 @@
 export LC_ALL=C
 export HOME=/config
 
-exec /usr/bin/grass
+# exec /usr/bin/grass
+
+# 5. 프로세스가 종료되지 않도록 유지
+wait
 
 #ls /usr/bin/grass && dpkg -i /grass/grass.deb && sleep 3s
 #sleep 7s ; ls /config/xdg/data/io.getgrass.desktop/localstorage && wmctrl -r grass -b add,hidden   
